230283ec7deaec54798953fd92aa2d3352b9abb9
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-29 14:30:23 -0400

    Creado pipe ToggleCasePipe e importado en el componente CustomPage

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -1,9 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case-pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export default class CustomPage {}
+export default class CustomPage {
+
+  name = signal('Cristian Cajiao');
+
+  uppercase = signal(false);
+
+  public toggleUppercase(): void {
+    if (this.uppercase()) {
+      this.uppercase.set(false);
+    } else {
+      this.uppercase.set(true);
+    }
+  }
+}
