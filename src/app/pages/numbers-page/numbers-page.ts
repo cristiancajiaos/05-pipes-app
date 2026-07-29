48812f40f81c44441e580a7879101b3eff3dd244
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-29 11:03:44 -0400

    Llenada NumbersPage con pipes de número

--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export default class NumbersPage {}
+export default class NumbersPage {
+
+  totalSells = signal(24_233_232.5567);
+  percent = signal(0.4856);
+}
