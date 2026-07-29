a9db4faeaae3de962a8171ddea7e8a1bf0e2ebf9
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-29 12:28:49 -0400

    Creado componente Card; agregadas muestras de pipes i18nSelect, i18nPlural, y Slice

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,9 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Card } from '../../components/card/card';
+import { I18nPluralPipe, I18nSelectPipe, SlicePipe } from '@angular/common';
 
+
+const client1 = {
+  name: 'John',
+  gender: 'male',
+  age:40,
+  address: 'San Francisco, EEUU'
+};
+
+const client2 = {
+  name: 'Carla',
+  gender: 'female',
+  age:42,
+  address: 'Santiago, Chile'
+};
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [Card, I18nSelectPipe, I18nPluralPipe, SlicePipe],
   templateUrl: './uncommon-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export default class UncommonPage {}
+export default class UncommonPage {
+
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+  //i18n Plural
+  clients = signal(['Maria', 'Pedro', 'Fernando', 'Melissa', 'Natalia', 'Andres', 'Juan', 'Carlos']);
+
+  clientsMap = signal({
+    '=0': 'No tenemos ningún cliente esperando',
+    '=1': 'Tenemos un cliente esperando',
+    '=2': 'Tenemos 2 clientes esperando',
+    'other': 'Tenemos # clientes esperando'
+  });
+
+  deleteClient() {
+    this.clients.update(
+      prev => prev.slice(1)
+    )
+  }
+}
